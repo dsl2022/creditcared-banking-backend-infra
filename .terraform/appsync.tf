efd72e1872e3fc5c3b66aace627b2e8dd5da3fbf
dsl2022 <2015rpro@gmail.com>
2024-01-09 00:22:01 -0500

    added serverless and webpack

--- .terraform/appsync.tf
+++ .terraform/appsync.tf
@@ -1,6 +1,5 @@
 resource "aws_appsync_graphql_api" "api" {
   name   = "example"
   schema = file("${path.module}/schema.graphql")
-
   authentication_type = "API_KEY"
 }
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  mode: 'development',
+  externals: [nodeExternals()],
+  optimization: {
+    minimize: false
+  },
+  performance: {
+    hints: false
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: { node: '12' } }]]
+          }
+        }
+      }
+    ]
+  }
+};
