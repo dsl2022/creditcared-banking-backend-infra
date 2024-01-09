7ee4dff1feb2f4fac2941f0aef83c81b9210e3e8
dsl2022 <2015rpro@gmail.com>
2024-01-09 08:29:07 -0500

    removed unwanted zip

--- .terraform/appsync.tf
+++ .terraform/appsync.tf
@@ -1,5 +1,5 @@
 resource "aws_appsync_graphql_api" "api" {
-  name   = "example"  
+  name   = "core-appsync"  
   schema = file("${path.module}/../schema/schema.graphql")
   authentication_type = "API_KEY"
 }
