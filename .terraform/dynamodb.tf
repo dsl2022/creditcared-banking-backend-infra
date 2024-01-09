97257c88f91d1fd634c7c3d5ebf8824432e1a0c2
dsl2022 <2015rpro@gmail.com>
2024-01-09 00:57:28 -0500

    removed unwanted zip

--- .terraform/dynamodb.tf
+++ .terraform/dynamodb.tf
@@ -3,7 +3,9 @@ resource "aws_dynamodb_table" "table" {
   hash_key       = "id"
   stream_enabled = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
-
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
   attribute {
     name = "id"
     type = "S"
