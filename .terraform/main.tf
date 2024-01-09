a36a61e6281807db0b7bbe2823429df426d3e3ad
dsl2022 <2015rpro@gmail.com>
2024-01-09 00:24:17 -0500

    removed unwanted zip

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -1,15 +1,1 @@
-module "appsync" {
-  source = "./modules/appsync"
-}
-
-module "dynamodb" {
-  source = "./modules/dynamodb"
-}
-
-module "lambda" {
-  source = "./modules/lambda"
-}
-
-module "sqs" {
-  source = "./modules/sqs"
-}
+data "aws_caller_identity" "current" {}
