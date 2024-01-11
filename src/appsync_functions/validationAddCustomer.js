d133e5890313c801c129adafdf7d7435f4e02f4d
dsl2022 <2015rpro@gmail.com>
2024-01-11 16:18:21 -0500

    updated ddb config and added graphql schema for add customers

--- src/appsync_functions/validationAddCustomer.js
+++ src/appsync_functions/validationAddCustomer.js
@@ -4,20 +4,31 @@ import { util } from '@aws-appsync/utils';
  * Called before the request function of the first AppSync function in the pipeline.
  *  @param ctx the context object holds contextual information about the function invocation.
  */
-export function request(ctx) {
-  console.log("test ctx",ctx)   
+export function request(ctx) {  
+  const input = ctx.arguments.input;  
+  const attributeValues = util.dynamodb.toMapValues(input);
   ctx.stash.allowedGroups = ['admin'];
   ctx.stash.startedAt = util.time.nowISO8601();
-  return {};
+   return {
+     operation: 'PutItem',
+    key: util.dynamodb.toMapValues({ id: input.accountToken }), // Make sure `id` is part of your input
+    attributeValues,
+  };
 }
+
 /**
  * Called after the response function of the last AppSync function in the pipeline.
  * @param ctx the context object holds contextual information about the function invocation.
  */
-export function response(ctx) {
-  const result = [];
-  for (const item of ctx.prev.result) {
-    if (ctx.stash.allowedGroups.indexOf(item.group) > -1) result.push(item);
+export function response(ctx) {    
+  const { error, result } = ctx;
+  if (error) {
+    if (!ctx.stash.errors) ctx.stash.errors = []
+    ctx.stash.errors.push(ctx.error)
+    return util.appendError(error.message, error.type, result);
   }
-  return result;
+  return {
+    success:true,
+    message:"application was submitted successfully",    
+    };
 }
