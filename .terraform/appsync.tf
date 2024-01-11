092667637d0f139aeb65f6d14593ed2143d41c03
dsl2022 <2015rpro@gmail.com>
2024-01-10 22:37:19 -0500

    updated ddb config and added graphql schema for add customers

--- .terraform/appsync.tf
+++ .terraform/appsync.tf
@@ -1,5 +1,5 @@
 resource "aws_appsync_graphql_api" "core_api" {
-  name   = "core-b-appsync"  
+  name   = "core-banking-appsync"  
   schema = file("${path.module}/../schema/schema.graphql")
   authentication_type = "API_KEY"
 }
