963a3462d203d8551fde8e2fc1735c0bb366af06
dsl2022 <2015rpro@gmail.com>
2024-01-10 22:35:27 -0500

    updated ddb config and added graphql schema for add customers

--- .terraform/resolvers.tf
+++ .terraform/resolvers.tf
@@ -3,10 +3,10 @@ resource "aws_appsync_resolver" "add_customer_validation" {
   api_id = aws_appsync_graphql_api.core_api.id
   field  = "applyForCreditCard"
   kind   = "PIPELINE"
-  runtime {
-    name            = "APPSYNC_JS"
-    runtime_version = "1.0.0"
-  }
+#   runtime {
+#     name            = "APPSYNC_JS"
+#     runtime_version = "1.0.0"
+#   }
 
   pipeline_config {
     functions = [
