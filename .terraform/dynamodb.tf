70a7493c5a72c301424101c70934b0f441b2f706
dsl2022 <2015rpro@gmail.com>
2024-01-10 20:25:03 -0500

    updated ddb config and added graphql schema for add customers

--- .terraform/dynamodb.tf
+++ .terraform/dynamodb.tf
@@ -1,13 +1,24 @@
 resource "aws_dynamodb_table" "table" {
   name           = "core-res-ddb"
-  hash_key       = "id"
+  hash_key       = "accountToken"
   stream_enabled = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
   attribute {
-    name = "id"
+    name = "accountToken"
     type = "S"
+  }
+  attribute {
+    name = "annualIncome"
+    type = "S"
+  }
+   global_secondary_index {
+    name               = "AnnualIncome"
+    hash_key           = "annualIncome"
+    projection_type    = "ALL"
+    read_capacity      = 10
+    write_capacity     = 10
   }
 }
