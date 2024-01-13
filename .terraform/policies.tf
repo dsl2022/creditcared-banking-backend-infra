adc9b326b05983c66a9be91f4562b54c69365362
dsl2022 <2015rpro@gmail.com>
2024-01-12 23:58:13 -0500

    added failed stream processor lambda, and cloudwatch logs as well sqs for the failed stream messages

--- .terraform/policies.tf
+++ .terraform/policies.tf
@@ -35,12 +35,56 @@ resource "aws_iam_role_policy" "lambda_logging" {
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents",
-          "sqs:*",
-          "dyamodb:*"
         ],
         Effect = "Allow",
         Resource = "arn:aws:logs:*:*:*"
       },
     ]
   })
+}
+
+# IAM policy for SQS access
+resource "aws_iam_policy" "lambda_sqs_policy" {
+  name   = "lambda-sqs-access"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ],
+        Resource = "*" 
+      }
+    ]
+  })
+}
+
+# IAM policy for SQS access
+resource "aws_iam_policy" "lambda_dynamodb_policy" {
+  name   = "lambda-ddb-access"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:*",
+        ],
+        Resource = "*" 
+      }
+    ]
+  })
+}
+# Attach the SQS policy to the Lambda execution role
+resource "aws_iam_role_policy_attachment" "lambda_sqs_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_sqs_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
 }
