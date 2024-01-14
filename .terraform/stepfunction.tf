0f437dd4b3a73a293fc3b388bfca807ab9196fc0
dsl2022 <2015rpro@gmail.com>
2024-01-14 14:15:09 -0500

    added sqs for all steps for stepfunc

--- .terraform/stepfunction.tf
+++ .terraform/stepfunction.tf
@@ -1,7 +1,7 @@
 resource "aws_sfn_state_machine" "credit_card_application_state_machine" {
   name     = "creditcard-application"
   role_arn = aws_iam_role.step_function_role.arn
-  definition = templatefile("${path.module}/../templates/credit-card-app-stfn-def.json",{
+  definition = templatefile("${path.module}/../templates/credit-card-app-stfn-def.tpl",{
     ValidateApplicationQueue = "${aws_sqs_queue.validate-application.url}"
     CheckIdentityQueue = "${aws_sqs_queue.check-identity.url}"
     CheckCreditScoreQueue = "${aws_sqs_queue.check-credit-score.url}"
